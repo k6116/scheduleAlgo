e0cdf38c6946269307a71d30d7f19b79a98e0a24
k6116 <naekyung@gmail.com>
2018-08-15 01:23:18 -0700

    Update school_2.py

--- school_2.py
+++ school_2.py
@@ -14,15 +14,13 @@ from ortools.sat.python import cp_model
 class SchoolSchedulingProblem(object):
 
   def __init__(self, subjects, teachers, curriculum, specialties, working_days,
-               periods, levels, sections, teacher_work_hours):
+               periods, teacher_work_hours):
     self.subjects = subjects
     self.teachers = teachers
     self.curriculum = curriculum
     self.specialties = specialties
     self.working_days = working_days
     self.periods = periods
-    self.levels = levels
-    self.sections = sections
     self.teacher_work_hours = teacher_work_hours
 
 
@@ -44,85 +42,66 @@ class SchoolSchedulingSatSolver(object):
     self.num_slots = len(self.timeslots)
     self.num_teachers = len(problem.teachers)
     self.num_subjects = len(problem.subjects)
-    self.num_levels = len(problem.levels)
-    self.num_sections = len(problem.sections)
-    self.courses = [
-        x * self.num_levels + y
-        for x in problem.levels
-        for y in problem.sections
-    ]
-    self.num_courses = self.num_levels * self.num_sections
 
-    all_courses = range(self.num_courses)
     all_teachers = range(self.num_teachers)
     all_slots = range(self.num_slots)
-    all_sections = range(self.num_sections)
     all_subjects = range(self.num_subjects)
-    all_levels = range(self.num_levels)
 
     self.model = cp_model.CpModel()
 
     self.assignment = {}
-    for c in all_courses:
-      for s in all_subjects:
-        for t in all_teachers:
-          for slot in all_slots:
-            if t in self.problem.specialties[s]:
-              name = 'C:{%i} S:{%i} T:{%i} Slot:{%i}' % (c, s, t, slot)
-              self.assignment[c, s, t, slot] = self.model.NewBoolVar(name)
-            else:
-              name = 'NO DISP C:{%i} S:{%i} T:{%i} Slot:{%i}' % (c, s, t, slot)
-              self.assignment[c, s, t, slot] = self.model.NewIntVar(0, 0, name)
+
+    for s in all_subjects:
+      for t in all_teachers:
+        for slot in all_slots:
+          if t in self.problem.specialties[s]:
+            name = 'S:{%i} T:{%i} Slot:{%i}' % (s, t, slot)
+            self.assignment[s, t, slot] = self.model.NewBoolVar(name)
+          else:
+            name = 'NO DISP  S:{%i} T:{%i} Slot:{%i}' % (s, t, slot)
+            self.assignment[s, t, slot] = self.model.NewIntVar(0, 0, name)
 
     # Constraints
 
     # Each course must have the quantity of classes specified in the curriculum
-    for level in all_levels:
-      for section in all_sections:
-        course = level * self.num_sections + section
-        for subject in all_subjects:
-          required_slots = self.problem.curriculum[self.problem.levels[
-              level], self.problem.subjects[subject]]
-          self.model.Add(
-              sum(self.assignment[course, subject, teacher, slot]
-                  for slot in all_slots
-                  for teacher in all_teachers) == required_slots)
+    # for level in all_levels:
+    #   for section in all_sections:
+    #     course = level * self.num_sections + section
+    #     for subject in all_subjects:
+    #       required_slots = self.problem.curriculum[self.problem.levels[
+    #           level], self.problem.subjects[subject]]
+    #       self.model.Add(
+    #           sum(self.assignment[course, subject, teacher, slot]
+    #               for slot in all_slots
+    #               for teacher in all_teachers) == required_slots)
 
     # Teacher can do at most one class at a time
     for teacher in all_teachers:
       for slot in all_slots:
         self.model.Add(
-            sum([
-                self.assignment[c, s, teacher, slot]
-                for c in all_courses
-                for s in all_subjects
-            ]) <= 1)
+            sum([self.assignment[s, teacher, slot] for s in all_subjects]) <= 1)
 
     # Maximum work hours for each teacher
     for teacher in all_teachers:
       self.model.Add(
-          sum([
-              self.assignment[c, s, teacher, slot] for c in all_courses
-              for s in all_subjects for slot in all_slots
-          ]) <= self.problem.teacher_work_hours[teacher])
+          sum([self.assignment[s, teacher, slot] for s in all_subjects for slot in all_slots]) <= self.problem.teacher_work_hours[teacher])
 
-    # Teacher makes all the classes of a subject's course
-    teacher_courses = {}
-    for level in all_levels:
-      for section in all_sections:
-        course = level * self.num_sections + section
-        for subject in all_subjects:
-          for t in all_teachers:
-            name = 'C:{%i} S:{%i} T:{%i}' % (course, subject, teacher)
-            teacher_courses[course, subject, t] = self.model.NewBoolVar(name)
-            temp_array = [
-                self.assignment[course, subject, t, slot] for slot in all_slots
-            ]
-            self.model.AddMaxEquality(teacher_courses[course, subject, t],
-                                      temp_array)
-          self.model.Add(
-              sum(teacher_courses[course, subject, t]
-                  for t in all_teachers) == 1)
+    # # Teacher makes all the classes of a subject's course
+    # teacher_courses = {}
+    # for level in all_levels:
+    #   for section in all_sections:
+    #     for subject in all_subjects:
+    #       for t in all_teachers:
+    #         name = 'S:{%i} T:{%i}' % (subject, teacher)
+    #         teacher_courses[subject, t] = self.model.NewBoolVar(name)
+    #         temp_array = [
+    #             self.assignment[subject, t, slot] for slot in all_slots
+    #         ]
+    #         self.model.AddMaxEquality(teacher_courses[subject, t],
+    #                                   temp_array)
+    #       self.model.Add(
+    #           sum(teacher_courses[course, subject, t]
+    #               for t in all_teachers) == 1)
 
     # Solution collector
     self.collector = None
@@ -133,7 +112,7 @@ class SchoolSchedulingSatSolver(object):
 
     solver = cp_model.CpSolver()
     solution_printer = SchoolSchedulingSatSolutionPrinter(self.assignment, self.problem.working_days, self.problem.periods, self.timeslots,
-      self.problem.teachers, self.problem.subjects, self.problem.levels, self.problem.sections, self.num_courses, a_few_solutions)
+      self.problem.teachers, self.problem.subjects, a_few_solutions)
     status = solver.SearchForAllSolutions(self.model, solution_printer)
     print('- Statistics')
     print('  - Branches', solver.NumBranches())
@@ -147,15 +126,13 @@ class SchoolSchedulingSatSolver(object):
 
 class SchoolSchedulingSatSolutionPrinter(cp_model.CpSolverSolutionCallback):
 
-  def __init__(self, assignment, working_days, periods, timeslots, teachers, subjects, levels, sections, num_courses, sols):
+  def __init__(self, assignment, working_days, periods, timeslots, teachers, subjects, sols):
 
     self.__working_days = working_days
     self.__periods = periods
     self.__slots = timeslots
     self.__teachers = teachers
     self.__subjects = subjects
-    self.__levels = levels
-    self.__sections = sections
 
     self.__assignment = assignment
     self.__num_days = len(working_days)
@@ -163,9 +140,6 @@ class SchoolSchedulingSatSolutionPrinter(cp_model.CpSolverSolutionCallback):
     self.__num_slots = len(timeslots)
     self.__num_teachers = len(teachers)
     self.__num_subjects = len(subjects)
-    self.__num_levels = len(levels)
-    self.__num_sections = len(sections)
-    self.__num_courses = self.__num_levels * self.__num_sections
     self.__solutions = set(sols)
     self.__solution_count = 0
 
@@ -175,15 +149,14 @@ class SchoolSchedulingSatSolutionPrinter(cp_model.CpSolverSolutionCallback):
       print('\n')
       print('Solution #%i' % self.__solution_count)
       
-      for c in range(self.__num_courses):
-        for s in range(self.__num_subjects):
-          for t in range(self.__num_teachers):
-            for ts in range(self.__num_slots):
-              if self.Value(self.__assignment[(c, s, t, ts)]):
-                subject_name = self.__subjects[s]
-                teacher_name = self.__teachers[t]
-                slot_name = self.__slots[ts]
-                print(' Course #%i | Subject #%s | Teacher #%s | TimeSlot #%s' % (c, subject_name, teacher_name, slot_name))
+      for s in range(self.__num_subjects):
+        for t in range(self.__num_teachers):
+          for ts in range(self.__num_slots):
+            if self.Value(self.__assignment[(s, t, ts)]):
+              subject_name = self.__subjects[s]
+              teacher_name = self.__teachers[t]
+              slot_name = self.__slots[ts]
+              print(' Subject #%s | Teacher #%s | TimeSlot #%s' % (subject_name, teacher_name, slot_name))
       print('\n')
 
   def SolutionCount(self):
@@ -193,8 +166,6 @@ class SchoolSchedulingSatSolutionPrinter(cp_model.CpSolverSolutionCallback):
 def main():
   # DATA
   subjects = ['English', 'Math', 'History']
-  levels = ['1-', '2-', '3-']
-  sections = ['A']
   teachers = ['Mario', 'Elvis', 'Donald', 'Ian']
   teachers_work_hours = [18, 12, 12, 18]
   working_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
@@ -216,28 +187,10 @@ def main():
       [0, 3],  # Math      -> Mario & Ian
       [2, 3]  # History   -> Donald & Ian
   ]
-  
-  # subjects = ['English', 'Math']
-  # levels = ['1-']
-  # sections = ['A']
-  # teachers = ['Mario', 'Elvis']
-  # teachers_work_hours = [18, 12]
-  # working_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-  # periods = ['08:00-09:30']
-  # curriculum = {
-  #   ('1-', 'English'): 3,
-  #   ('1-', 'Math'): 3
-  # }
-  # specialties_idx_inverse = [
-  #     [0],  # English   -> Elvis & Ian
-  #     [1]  # Math      -> Mario & Ian
-  # ]
-
- 
 
   problem = SchoolSchedulingProblem(
       subjects, teachers, curriculum, specialties_idx_inverse, working_days,
-      periods, levels, sections, teachers_work_hours)
+      periods, teachers_work_hours)
   solver = SchoolSchedulingSatSolver(problem)
   solver.solve()
   solver.print_status()
